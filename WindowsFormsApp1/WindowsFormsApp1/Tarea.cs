81b9b7fcc24953fa76ec831981b9232806c8adf8
Chicabytes <chocobarjulietain@gmail.com>
2020-09-04 19:43:03 -0300

    Primer intento

---
+++ WindowsFormsApp1/WindowsFormsApp1/Tarea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public class Tarea
+    {
+        private int id;
+        private string descripcion;
+        private int duracion;
+
+        public int Id { get => Id; set => Id = value; }
+        public string Descripcion { get => Descripcion; set => Descripcion = value; }
+        public int Duracion { get => Duracion; set => Duracion = value; }
+
+
+    }
+}
